055f60d120f2a59975edbf431c2d7a9db7ff5a2a
Elias Reuter <reuter@smark.de>
2022-04-08 12:30:08 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
   FieldExtensionFeature,
   ExtensionPermission,
   useFieldExtension,
-  useUiExtension,
 } from '@graphcms/uix-react-sdk';
 
 // use the ExtensionDeclaration type to validate the shape of your declaration object
@@ -28,19 +27,9 @@ function App() {
     field
   } = useFieldExtension();
 
-  const [storeIds, setStoreIds] = useState([]);
   const [graphcmsId, setGraphCmsId] = useState([]);
 
   useEffect(() => {
-    fetch("https://api-eu-central-1.graphcms.com/v2/cl1eyp2gh2pc901z7amoq8axs/master?query=query%7B%0A%20%20storeArticles%7B%0A%20%20%20%20storeId%0A%20%20%20%20articleId%0A%20%20%7D%0A%7D")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setStoreIds(result.data.storeArticles);
-      },
-      (error) => {
-      }
-    );
     const unsubscribe = async () => {
       await subscribeToFieldState(
         field.apiId,
@@ -50,8 +39,7 @@ function App() {
         { value: true },
       );
     };
-    return () => unsubscribe();
-
+    //return () => unsubscribe();
 }, [subscribeToFieldState, field.apiId]);
 
   return(
