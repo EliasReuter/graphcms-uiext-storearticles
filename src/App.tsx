8bb49055d51a6e3aa9ab9f6f01b3c9691b8e6e84
Elias Reuter <reuter@smark.de>
2022-04-14 13:03:38 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -92,120 +92,121 @@ function App() {
   ];
 
   return (
-    <div className={"ml-10 mr-10"}>
-      <p className={"text-5xl mt-10"}>STORE ARTICLES</p>
-      <p className={"text-2xl mt-6"}>GraphCMS</p>
-      <div>
-        {articleFields.map((row) => {
-          return (
-            <div className={"mt-2 flex gap-4"}>
-              {row.map((article) => {
-                return article.type === "boolean" ? (
-                  <div className={"w-64"}>
-                    <p>{article.label}</p>
-                    <Checkbox
-                      disabled={disableArticleInputs}
+    <Wrapper declaration={declaration}>
+      <div className={"ml-10 mr-10"}>
+        <p className={"text-5xl mt-10"}>STORE ARTICLES</p>
+        <p className={"text-2xl mt-6"}>GraphCMS</p>
+        <div>
+          {articleFields.map((row) => {
+            return (
+              <div className={"mt-2 flex gap-4"}>
+                {row.map((article) => {
+                  return article.type === "boolean" ? (
+                    <div className={"w-64"}>
+                      <p>{article.label}</p>
+                      <Checkbox
+                        disabled={disableArticleInputs}
+                        key={article.label}
+                        onChange={(ev) => article.func(ev.target.checked)}
+                      />
+                    </div>
+                  ) : (
+                    <TextField
                       key={article.label}
-                      onChange={(ev) => article.func(ev.target.checked)}
+                      label={article.label}
+                      type={article.type}
+                      fullWidth={true}
+                      disabled={disableArticleInputs}
+                      multiline={row.length === 1}
+                      rows={4}
+                      onChange={(ev) => article.func(ev.target.value)}
                     />
-                  </div>
-                ) : (
-                  <TextField
-                    key={article.label}
-                    label={article.label}
-                    type={article.type}
-                    fullWidth={true}
-                    disabled={disableArticleInputs}
-                    multiline={row.length === 1}
-                    rows={4}
-                    onChange={(ev) => article.func(ev.target.value)}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div className={"flex gap-3 mt-5 mb-10"}>
-        <Button
-          variant="contained"
-          disabled={disableArticleInputs}
-          onClick={() => {
-            try {
-              createArticle({
-                variables: {
-                  articleId: Number(_articleId),
-                  ageRestriction: Number(_ageRestriction),
-                  price: Number(_price),
-                  name: _name,
-                  description: _description,
-                },
-              }).then((result) => {
-                setRealArticleId(result.data.createArticle.id);
-                publishArticle({
-                  variables: { articleId: articleId },
-                }).then((result) => {
-                  console.log(result);
-                  setDisableArticleInputs(true);
-                });
-              });
-            } catch (ex) {
-              alert("Error while trying to create Store Article");
-            }
-          }}
-        >
-          Create Article
-        </Button>
-      </div>
-      <div className={"mb-10"}>
-        <p className={"text-2xl mt-6"}>Select Stores to add "{_name}"</p>
-        <div className={"flex gap-5"}>
-          {data?.stores?.map((x: any, index: number) => (
-            <div className={"mb-5"}>
-              <Checkbox
-                disabled={!disableArticleInputs}
-                key={x.storeId}
-                checked={true}
-                onChange={(ev) => {
-                  if (ev.target.value) _stores.push(x.storeId);
-                }}
-              ></Checkbox>
-              <p>{x.name}</p>
-            </div>
-          ))}
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
-        <Button
-          variant="contained"
-          disabled={!disableArticleInputs}
-          onClick={() => {
-            try {
-              _stores.map((str) => {
-                createStoreArticle({
+        <div className={"flex gap-3 mt-5 mb-10"}>
+          <Button
+            variant="contained"
+            disabled={disableArticleInputs}
+            onClick={() => {
+              try {
+                createArticle({
                   variables: {
                     articleId: Number(_articleId),
-                    storeId: str,
+                    ageRestriction: Number(_ageRestriction),
+                    price: Number(_price),
+                    name: _name,
+                    description: _description,
                   },
                 }).then((result) => {
-                  console.log(result);
-                  publishStoreArticle({
-                    variables: { sAID: result.data.createStoreArticle.id },
+                  setRealArticleId(result.data.createArticle.id);
+                  publishArticle({
+                    variables: { articleId: articleId },
                   }).then((result) => {
-                    setDisableArticleInputs(false);
                     console.log(result);
+                    setDisableArticleInputs(true);
                   });
                 });
-              });
-            } catch (ex) {
-              alert("Error while trying to create the Store Article");
-            }
-          }}
-        >
-          Add Article to Stores
-        </Button>
+              } catch (ex) {
+                alert("Error while trying to create Store Article");
+              }
+            }}
+          >
+            Create Article
+          </Button>
+        </div>
+        <div className={"mb-10"}>
+          <p className={"text-2xl mt-6"}>Select Stores to add "{_name}"</p>
+          <div className={"flex gap-5"}>
+            {data?.stores?.map((x: any, index: number) => (
+              <div className={"mb-5"}>
+                <Checkbox
+                  disabled={!disableArticleInputs}
+                  key={x.storeId}
+                  checked={true}
+                  onChange={(ev) => {
+                    if (ev.target.value) _stores.push(x.storeId);
+                  }}
+                ></Checkbox>
+                <p>{x.name}</p>
+              </div>
+            ))}
+          </div>
+          <Button
+            variant="contained"
+            disabled={!disableArticleInputs}
+            onClick={() => {
+              try {
+                _stores.map((str) => {
+                  createStoreArticle({
+                    variables: {
+                      articleId: Number(_articleId),
+                      storeId: str,
+                    },
+                  }).then((result) => {
+                    console.log(result);
+                    publishStoreArticle({
+                      variables: { sAID: result.data.createStoreArticle.id },
+                    }).then((result) => {
+                      setDisableArticleInputs(false);
+                      console.log(result);
+                    });
+                  });
+                });
+              } catch (ex) {
+                alert("Error while trying to create the Store Article");
+              }
+            }}
+          >
+            Add Article to Stores
+          </Button>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
-  // </Wrapper>
 }
 
 export default App;
