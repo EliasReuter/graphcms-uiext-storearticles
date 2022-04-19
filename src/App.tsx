7112cdece4192131c844ecbd097993612db1f701
Elias Reuter <reuter@smark.de>
2022-04-19 09:47:21 +0200

    wrapper addin

--- src/App.tsx
+++ src/App.tsx
@@ -121,200 +121,205 @@ function App() {
   >(null);
 
   return (
-    <div className={"ml-10 mr-10"}>
-      <div className={"mb-5"}>
-        <p className={"text-5xl mt-10"}>STORE ARTICLES</p>
-        <p className={"text-2xl mt-6"}>GraphCMS</p>
-        <p className={logColor}>{log}</p>
-      </div>
-      <Button
-        disabled={!disableArticleInputs || !disableStoreArticleInputs}
-        onClick={() => {
-          setDisableArticleInputs(false);
-        }}
-      >
-        + Add Article
-      </Button>
-      <div>
-        {articleFields.map((row) => {
-          return (
-            <div className={"mt-2 flex gap-4"}>
-              {row.map((article) => {
-                if (article.type === "boolean") {
-                  return (
-                    <div className={"w-64"}>
-                      <p>{article.label}</p>
-                      <Checkbox
-                        disabled={disableArticleInputs}
-                        key={article.label}
-                        onChange={(ev) => article.func(ev.target.checked)}
-                      />
-                    </div>
-                  );
-                } else if (article.type === "autocomplete") {
-                  return (
-                    <TextField
-                      label={article.label}
-                      onChange={(ev) => article.func(ev.target.value)}
-                      type={article.type}
-                      fullWidth={true}
-                      disabled={disableArticleInputs}
-                      multiline={row.length === 1}
-                      rows={4}
-                      key={article.label}
-                    />
-                  );
-                } else {
-                  return (
-                    <TextField
-                      key={article.label}
-                      label={article.label}
-                      type={article.type}
-                      fullWidth={true}
-                      disabled={disableArticleInputs}
-                      multiline={row.length === 1}
-                      rows={4}
-                      onChange={(ev) => article.func(ev.target.value)}
-                    />
-                  );
-                }
-              })}
-            </div>
-          );
-        })}
-      </div>
-      <div className={"flex gap-3 mt-5 mb-10"}>
-        <Button
-          variant="contained"
-          disabled={disableArticleInputs}
-          onClick={() => {
-            if (_articleId === -1) {
-              setLog("Please complete Article Id field");
-              return;
-            }
-            if (_name === "") {
-              setLog("Please complete Name field");
-              return;
-            }
-            if (_description === "") {
-              setLogText("Please complete Description field", "text-red-600");
-              return;
-            }
-            if (_ageRestriction === -1) {
-              setLogText(
-                "Please complete Age Restriction field",
-                "text-red-600"
-              );
-              return;
-            }
-            if (_price === -1) {
-              setLog("Please complete Price field");
-              return;
-            }
-            createArticle({
-              variables: {
-                articleId: Number(_articleId),
-                ageRestriction: Number(_ageRestriction),
-                price: Number(_price),
-                name: _name,
-                description: _description,
-              },
-            })
-              .then((result) => {
-                setLogText("Successful creation of Article", "text-green-600");
-                setRealArticleId(result.data.createArticle.id);
-                publishArticle({
-                  variables: { articleId: articleId },
-                })
-                  .then((result) => {
-                    setLogText(
-                      "Successful publishing of Article",
-                      "text-green-600"
-                    );
-                    setDisableStoreArticleInputs(false);
-                    setDisableArticleInputs(true);
-                  })
-                  .catch((reason) => {
-                    setLogText(
-                      "Error while trying to publish Article",
-                      "text-red-600"
-                    );
-                  });
-              })
-              .catch((reason) => {
-                setLogText(
-                  "Error while trying to create Article",
-                  "text-red-600"
-                );
-              });
-          }}
-        >
-          Create Article
-        </Button>
-      </div>
-      <div className={"mb-10"}>
-        <p className={"text-2xl mt-6"}>Select Stores to add "{_name}"</p>
-        <div className={"flex gap-5"}>
-          {data?.stores?.map((x: any, index: number) => (
-            <div className={"mb-5"}>
-              <Checkbox
-                disabled={disableStoreArticleInputs}
-                key={x.storeId}
-                checked={true}
-                onChange={(ev) => {
-                  if (ev.target.value) _stores.push(x.storeId);
-                }}
-              ></Checkbox>
-              <p>{x.name}</p>
-            </div>
-          ))}
+    <Wrapper declaration={declaration}>
+      <div className={"ml-10 mr-10"}>
+        <div className={"mb-5"}>
+          <p className={"text-5xl mt-10"}>STORE ARTICLES</p>
+          <p className={"text-2xl mt-6"}>GraphCMS</p>
+          <p className={logColor}>{log}</p>
         </div>
         <Button
-          variant="contained"
-          disabled={disableStoreArticleInputs}
+          disabled={!disableArticleInputs || !disableStoreArticleInputs}
           onClick={() => {
-            _stores.map((str) => {
-              createStoreArticle({
+            setDisableArticleInputs(false);
+          }}
+        >
+          + Add Article
+        </Button>
+        <div>
+          {articleFields.map((row) => {
+            return (
+              <div className={"mt-2 flex gap-4"}>
+                {row.map((article) => {
+                  if (article.type === "boolean") {
+                    return (
+                      <div className={"w-64"}>
+                        <p>{article.label}</p>
+                        <Checkbox
+                          disabled={disableArticleInputs}
+                          key={article.label}
+                          onChange={(ev) => article.func(ev.target.checked)}
+                        />
+                      </div>
+                    );
+                  } else if (article.type === "autocomplete") {
+                    return (
+                      <TextField
+                        label={article.label}
+                        onChange={(ev) => article.func(ev.target.value)}
+                        type={article.type}
+                        fullWidth={true}
+                        disabled={disableArticleInputs}
+                        multiline={row.length === 1}
+                        rows={4}
+                        key={article.label}
+                      />
+                    );
+                  } else {
+                    return (
+                      <TextField
+                        key={article.label}
+                        label={article.label}
+                        type={article.type}
+                        fullWidth={true}
+                        disabled={disableArticleInputs}
+                        multiline={row.length === 1}
+                        rows={4}
+                        onChange={(ev) => article.func(ev.target.value)}
+                      />
+                    );
+                  }
+                })}
+              </div>
+            );
+          })}
+        </div>
+        <div className={"flex gap-3 mt-5 mb-10"}>
+          <Button
+            variant="contained"
+            disabled={disableArticleInputs}
+            onClick={() => {
+              if (_articleId === -1) {
+                setLog("Please complete Article Id field");
+                return;
+              }
+              if (_name === "") {
+                setLog("Please complete Name field");
+                return;
+              }
+              if (_description === "") {
+                setLogText("Please complete Description field", "text-red-600");
+                return;
+              }
+              if (_ageRestriction === -1) {
+                setLogText(
+                  "Please complete Age Restriction field",
+                  "text-red-600"
+                );
+                return;
+              }
+              if (_price === -1) {
+                setLog("Please complete Price field");
+                return;
+              }
+              createArticle({
                 variables: {
                   articleId: Number(_articleId),
-                  storeId: str,
+                  ageRestriction: Number(_ageRestriction),
+                  price: Number(_price),
+                  name: _name,
+                  description: _description,
                 },
               })
                 .then((result) => {
-                  publishStoreArticle({
-                    variables: { sAID: result.data.createStoreArticle.id },
+                  setLogText(
+                    "Successful creation of Article",
+                    "text-green-600"
+                  );
+                  setRealArticleId(result.data.createArticle.id);
+                  publishArticle({
+                    variables: { articleId: articleId },
                   })
                     .then((result) => {
                       setLogText(
-                        "Successful creation of Store Articles",
-                        "text-black"
+                        "Successful publishing of Article",
+                        "text-green-600"
                       );
-                      setOpen(true);
-                      setDisableStoreArticleInputs(true);
+                      setDisableStoreArticleInputs(false);
                       setDisableArticleInputs(true);
                     })
                     .catch((reason) => {
                       setLogText(
-                        "Error while trying to publish Store Articles",
+                        "Error while trying to publish Article",
                         "text-red-600"
                       );
                     });
                 })
                 .catch((reason) => {
                   setLogText(
-                    "Error while trying to create Store Articles",
+                    "Error while trying to create Article",
                     "text-red-600"
                   );
                 });
-            });
-          }}
-        >
-          Add Article to Stores
-        </Button>
+            }}
+          >
+            Create Article
+          </Button>
+        </div>
+        <div className={"mb-10"}>
+          <p className={"text-2xl mt-6"}>Select Stores to add "{_name}"</p>
+          <div className={"flex gap-5"}>
+            {data?.stores?.map((x: any, index: number) => (
+              <div className={"mb-5"}>
+                <Checkbox
+                  disabled={disableStoreArticleInputs}
+                  key={x.storeId}
+                  checked={true}
+                  onChange={(ev) => {
+                    if (ev.target.value) _stores.push(x.storeId);
+                  }}
+                ></Checkbox>
+                <p>{x.name}</p>
+              </div>
+            ))}
+          </div>
+          <Button
+            variant="contained"
+            disabled={disableStoreArticleInputs}
+            onClick={() => {
+              _stores.map((str) => {
+                createStoreArticle({
+                  variables: {
+                    articleId: Number(_articleId),
+                    storeId: str,
+                  },
+                })
+                  .then((result) => {
+                    publishStoreArticle({
+                      variables: { sAID: result.data.createStoreArticle.id },
+                    })
+                      .then((result) => {
+                        setLogText(
+                          "Successful creation of Store Articles",
+                          "text-black"
+                        );
+                        setOpen(true);
+                        setDisableStoreArticleInputs(true);
+                        setDisableArticleInputs(true);
+                      })
+                      .catch((reason) => {
+                        setLogText(
+                          "Error while trying to publish Store Articles",
+                          "text-red-600"
+                        );
+                      });
+                  })
+                  .catch((reason) => {
+                    setLogText(
+                      "Error while trying to create Store Articles",
+                      "text-red-600"
+                    );
+                  });
+              });
+            }}
+          >
+            Add Article to Stores
+          </Button>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
-  //  </Wrapper>
+  //
 }
 
 export default App;
